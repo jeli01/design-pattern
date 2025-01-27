c026f21bfb3f3b72c25347e704dd8580afcd7a3a
jeli01 <jeli01@naver.com>
2025-01-28 02:33:35 +0900

    observer pattern 학습

---
+++ src/observer/DiceGame.java
@@ -0,0 +1,13 @@
+package observer;
+
+import java.util.LinkedList;
+
+public abstract class DiceGame {
+    protected LinkedList<Player> playerList = new LinkedList<Player>();
+
+    public void addPlayer(Player player) {
+        playerList.add(player);
+    }
+
+    public abstract void play();
+}
